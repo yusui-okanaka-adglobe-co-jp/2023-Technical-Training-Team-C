ac8baf3b351edc7b7df825b4fed16d469216f573
yusui-okanaka-adglobe-co-jp <130024649+yusui-okanaka-adglobe-co-jp@users.noreply.github.com>
2023-04-26 12:03:33 +0900

    Revert "共通部品_カレンダーダイアログの作成"

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -1,14 +1,11 @@
 <template>
   <default-layout page-name="教師用ページ">
     <!-- html記述場所 -->
-
     <TimetableComponent :timetables="timetables"></TimetableComponent>
-
   </default-layout>
 </template>
 
 <script lang="ts" setup>
-
 import { Timetable } from '~~/types/response/timetablesAcquireResponse'
 
 /* 検証用オブジェクト */
@@ -157,7 +154,6 @@ const timetables: Timetable[] = [
     ],
   },
 ]
-
 </script>
 
 <style scoped lang="scss"></style>
--- frontend/pages/timetableRegister.vue
+++ frontend/pages/timetableRegister.vue
@@ -1,31 +1,9 @@
 <template>
   <default-layout page-name="教師用登録ページ">
-    <h1>hoge</h1>
-    <p><button onclick="location.href='./home.html'">ホーム</button></p>
-    <p><button onclick="window.open('/studentHome.html')">生徒用画面確認</button></p>
-    <p><button class="logout" type="button">ログアウト</button></p>
-    <p>
-      開始日：<input type="date" name="example2" value="2022-04-18" /> 終了日：<input
-        type="date"
-        name="example2"
-        value="2022-04-18"
-      />
-    </p>
-    <p>
-      <button onclick="location.href='./home.html'">戻る</button>
-      <button onclick="location.href='./timetableUpdate.html'">時間割更新</button>
-    </p>
-
-    <dialog>
-      <p>これはモーダルダイアログです</p>
-      <button id="close">モーダルダイアログを閉じる</button>
-    </dialog>
-    <button id="show">モーダルダイアログを開く</button>
+    <!-- html記述場所 -->
   </default-layout>
 </template>
 
-<script lang="ts" setup>
-import '@/assets/css/common.css'
-</script>
+<script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped lang="scss"></style>
