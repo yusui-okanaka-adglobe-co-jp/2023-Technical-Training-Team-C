394d7208a7664c2a53415afb90f2a0aa829f9f0a
hiroka-sonobe-adglobe-co-jp <hiroka.sonobe@adglobe.co.jp>
2023-05-08 18:00:07 +0900

    TimetableSeeder作成

--- backend/database/seeders/TimetableSeeder.php
+++ backend/database/seeders/TimetableSeeder.php
@@ -28,7 +28,30 @@ class TimetableSeeder extends Seeder
                         'teacher_name' => '内藤',
                         'start_date' => '2023-04-01',
                         'end_date' => '2023-08-31',
-                        'created_at' => new DateTime(),
+                        'created_at' => '2023-01-01',
+                    ],
+                );
+                DB::table('timetables')->insert(
+                    [
+                        'day_of_week' => $dayOfWeekCount,
+                        'period' => $periodCount,
+                        'subject' => '英語',
+                        'teacher_name' => '佐々木',
+                        'start_date' => '2023-04-01',
+                        'end_date' => '2023-04-16',
+                        'created_at' => '2023-03-31',
+                    ],
+                );
+
+                DB::table('timetables')->insert(
+                    [
+                        'day_of_week' => $dayOfWeekCount,
+                        'period' => $periodCount,
+                        'subject' => '数学',
+                        'teacher_name' => '鈴木',
+                        'start_date' => '2023-04-04',
+                        'end_date' => '2023-04-10',
+                        'created_at' => '2023-04-03',
                     ],
                 );
             }
