eecdf21146f2df67ea875ccd3bceca0ac6d46938
shu-kimura-adglobe-co-jp <shu.kimura@adglobe.co.jp>
2023-05-08 12:52:25 +0900

    LoguoutController修正

--- backend/app/Http/Controllers/LogoutController.php
+++ backend/app/Http/Controllers/LogoutController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Cookie;
 
 class LogoutController extends Controller
 {
-    //ページ遷移の処理
+    //ログアウト処理
     public function __invoke(Request $request)
     {
         // Cookieの削除
