795b7d5907c0335019e3520852ae8498832d05eb
yusui-okanaka-adglobe-co-jp <yusui.okanaka@adglobe.co.jp>
2023-05-10 14:09:03 +0900

    作業中

--- backend/app/Http/Controllers/RegisterController.php
+++ backend/app/Http/Controllers/RegisterController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Timetable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\DB;
+use App\Models\Timetable;
+
+use PDOException;
+use Ramsey\Uuid\Type\Time;
 
 class RegisterController extends Controller
 {
@@ -12,10 +16,23 @@ class RegisterController extends Controller
     // POST ex) http://localhost:8000/api/timetablesCreate
     public function createTimetables(Request $request)
     {
-        echo "a";
-        // // $timetables = new Timetable();
+        // $timetables = new Timetable();
+        // echo $timetables;
         // $timetables->teacher_name = '山田';
-        // $timetables->save();
+        // echo $timetables;
+
+        $post = new Timetable();
+        $post->day_of_week = 3;
+        $post->period = 3;
+        $post->subject = '国語';
+        $post->teacher_name = '佐藤';
+        $post->start_date = '2023-05-05';
+        $post->end_date = '2023-05-06';
+        $post->save();
+        echo $post;
+
+        // DB::insert('insert into reviews (day_of_week) values (:day_of_week)', $param);
+
         $message = [
             'messages' => [
                 'success'
