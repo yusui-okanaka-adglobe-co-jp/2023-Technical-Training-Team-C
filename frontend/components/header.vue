fd0a58f65abf66da4faab1b8c11c47690e19c2f8
yusui-okanaka-adglobe-co-jp <yusui.okanaka@adglobe.co.jp>
2023-05-18 12:14:38 +0900

    ロゴ修正

--- frontend/components/header.vue
+++ frontend/components/header.vue
@@ -1,9 +1,11 @@
 <template>
   <header>
-    <img src="~/assets/img/logo.png" class="logo" />
-    <div class="titles">
-      <h1 class="font-size-xl">〇〇中学校時間割</h1>
-      <h2 class="font-size-l">{{ props.pageName }}</h2>
+    <div class="flex-box">
+      <div class="titles">
+        <h1 class="font-size-xl">〇〇中学校時間割</h1>
+        <h2 class="font-size-l">{{ props.pageName }}</h2>
+      </div>
+      <img src="~/assets/img/logo.png" class="logo" />
     </div>
   </header>
 </template>
@@ -19,30 +21,43 @@ header {
   width: 100%;
   height: 100px;
   background-color: #89929b;
-  div {
-    padding: 15px 0px 15px 100px;
-    h1 {
-      font-style: normal;
-    }
+  h1 {
+    font-style: normal;
   }
 }
+
+.flex-box {
+  display: flex;
+  position: relative;
+}
+.titles {
+  padding: 15px 0px 15px 100px;
+}
 .logo {
-  position: absolute;
-  width: 167px;
+  width: auto;
   height: 100px;
-  margin-left: 720px;
+  margin-right: auto;
+  margin-left: auto;
+  position: absolute;
+  left: 44%;
 }
 // モバイル用スタイル
 @media only screen and (max-width: 767px) {
   header {
     height: auto;
-    div {
-      margin: 0 auto;
-      padding: 15px 0px 15px 0px;
-      h1 {
-        text-align: center;
-      }
+
+    h1 {
+      text-align: center;
     }
+  }
+  .titles {
+    margin: 0 auto;
+    padding: 15px 0px 15px 0px;
+  }
+  .logo {
+    height: 100%;
+    margin-left: 0px;
+    left: 0%;
   }
 }
 </style>
