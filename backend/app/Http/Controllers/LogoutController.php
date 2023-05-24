e9ec8a09d3165f708857f75937e9a23124d1fa3a
yusui-okanaka-adglobe-co-jp <yusui.okanaka@adglobe.co.jp>
2023-05-24 17:50:16 +0900

    修正

--- backend/app/Http/Controllers/LogoutController.php
+++ backend/app/Http/Controllers/LogoutController.php
@@ -12,13 +12,15 @@ class LogoutController extends Controller
     {
         // Cookieの削除
         $response = response();
-        $response->setcookie('api_token', '', 0, '/');
-
-
-        // $response = response();
+        // $response->setcookie('api_token', '', 0, '/');
         // $response->cookie('api_token');
+        // $response->cookie('api_token', '', 0, '/');
+        Cookie::queue(Cookie::forget('api_token'));
+        // $response->cookie('api_token');
+        // レスポンスを返す
+        return;
+        // setcookie('api_token', '', 0, '/');
 
-        // return;
-        return $response;
+        // return $response;
     }
 }
